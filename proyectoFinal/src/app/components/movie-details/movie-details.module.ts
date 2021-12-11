d71219f0c15e6b4e275d631f8910076454b8166b
Luisina Fernandez Long <lfl@qbkconsulting.com>
2021-12-11 11:07:59 -0300

    Armo la page de detalle y sus estilos

--- proyectoFinal/src/app/components/movie-details/movie-details.module.ts
+++ proyectoFinal/src/app/components/movie-details/movie-details.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieDetailsComponent } from './movie-details.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [MovieDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [MovieDetailsComponent]
 })
